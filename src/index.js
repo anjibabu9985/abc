da0557caca1d6a423048ed945c4aaf0e369f111d
anjibabu <bandianjibabu8@gmail.com>
2018-10-09 16:40:32 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//import Secondscreen from './Secondscreen';
+//import Fourthscreen from './Fourthscreen';
+//import Thirdscreen from './Thirdscreen';
+//import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
